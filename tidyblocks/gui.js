f6761bb2fa09fa05554788964d6c64fa7f1f4002
Jaymie Rosen <jaymie_rosen@apple.com>
2019-10-31 17:26:36 -0400

    init commit

--- tidyblocks/gui.js
+++ tidyblocks/gui.js
@@ -370,16 +370,12 @@ const displayTab = (event, tabName) => {
  * Code for slider between blockly pane and tabs
  */
 // function is used for dragging and moving
-function dragElement( element, direction)
-{
+function dragElement( element, direction) {
     var   md; // remember mouse down info
     const first  = document.getElementById("first");
     const second = document.getElementById("second");
-    
     element.onmousedown = onMouseDown;
-    
-    function onMouseDown( e )
-    {
+    function onMouseDown(e){
   //console.log("mouse down: " + e.clientX);
   md = {e,
         offsetLeft:  element.offsetLeft,
@@ -393,23 +389,21 @@ function dragElement( element, direction)
   }
     }
     
-    function onMouseMove( e )
-    {
-  //console.log("mouse move: " + e.clientX);
-  var delta = {x: e.clientX - md.e.x,
-         y: e.clientY - md.e.y};
-  
-  if (direction === "H" ) // Horizontal
-  {
+  function onMouseMove(e){
+    //console.log("mouse move: " + e.clientX);
+    var delta = {x: e.clientX - md.e.x,
+    y: e.clientY - md.e.y};
+
+    if (direction === "H" ) { // Horizontal
       // prevent negative-sized elements
       delta.x = Math.min(Math.max(delta.x, -md.firstWidth),
-             md.secondWidth);
-      
+        md.secondWidth);
+
       element.style.left = md.offsetLeft + delta.x + "px";
       first.style.width = (md.firstWidth + delta.x) + "px";
       second.style.width = (md.secondWidth - delta.x) + "px";
-  }
     }
+  }
 }
 
 dragElement( document.getElementById("separator"), "H" );
